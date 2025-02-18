a1e29267ce26c9fea1f1f6f53a473bdf2e1b975d
Subhajit-14th <subhajitdtt700@gmail.com>
2025-02-18 22:40:12 +0530

    check the CI/CD

--- lib/viewModel/dashboard_screen_provider.dart
+++ lib/viewModel/dashboard_screen_provider.dart
@@ -1,16 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:liqueur_brooze/model/dashboardCouponItemsModel.dart';
 
-class DashboardScreenProvider extends ChangeNotifier {
-  final List<Dashboardcouponitemsmodel> _couponItems = [
-    Dashboardcouponitemsmodel(
-      couponType: 'Percentage',
-      couponCode: '926F',
-      value: '1600',
-      startDate: '12/03/2025',
-      endingDate: '26/04/2025',
-    ),
-  ];
-
-  List<Dashboardcouponitemsmodel> get couponItems => _couponItems;
-}
+class DashboardScreenProvider extends ChangeNotifier {}
