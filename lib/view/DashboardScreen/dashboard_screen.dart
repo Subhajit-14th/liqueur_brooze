4a426be1d90357ddd42304cfb97f82573bd26a34
Subhajit-14th <subhajitdtt700@gmail.com>
2025-04-01 19:46:44 +0530

    dash board screen design is updated

--- lib/view/DashboardScreen/dashboard_screen.dart
+++ lib/view/DashboardScreen/dashboard_screen.dart
@@ -10,262 +10,138 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
-    return Column(
-      children: [
-        /// Heading items and add coupon buttons
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'All Coupon',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Monserat',
-                    ),
-                  ),
-                  Text(
-                    'Manage your All Coupon',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: 'Monserat',
-                    ),
-                  ),
-                ],
-              ),
-              CommonButton(
-                width: 130,
-                height: 40,
-                buttonText: 'Add Coupon',
-                buttonColor: AppColor.primaryColor,
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AddCouponScreen()));
-                },
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 16),
+          buildDashboardCard(
+            title: "Sales",
+            subtitle: "Today",
+            value: "145",
+            percentage: "12%",
+            percentageColor: Colors.green,
+            icon: Icons.shopping_cart,
+            iconColor: Colors.blue,
+            changeText: "increase",
           ),
-        ),
-        SizedBox(height: height * 0.02),
+          SizedBox(height: 16), // Add spacing between cards
+          buildDashboardCard(
+            title: "Revenue",
+            subtitle: "This Month",
+            value: "\$3,264",
+            percentage: "8%",
+            percentageColor: Colors.green,
+            icon: Icons.attach_money,
+            iconColor: Colors.green,
+            changeText: "increase",
+          ),
+          SizedBox(height: 16),
+          buildDashboardCard(
+            title: "Customers",
+            subtitle: "This Year",
+            value: "1244",
+            percentage: "12%",
+            percentageColor: Colors.red,
+            icon: Icons.people,
+            iconColor: Colors.orange,
+            changeText: "decrease",
+          ),
+        ],
+      ),
+    );
+  }
 
-        /// Coupon Lists
-        Expanded(
-          child: ListView.builder(
-            itemCount: 10,
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            padding: EdgeInsets.all(16),
-            itemBuilder: (context, index) {
-              return Stack(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 16),
-                    padding: EdgeInsets.all(10),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(14),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withAlpha(50),
-                          blurRadius: 10,
-                          spreadRadius: 1,
-                        ),
-                      ],
-                    ),
-                    child: Row(
-                      spacing: 18,
-                      children: [
-                        /// Coupon Type Or Coupon Code
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            /// Coupon Type
-                            Text(
-                              'Coupon Type',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                            Text(
-                              'Percentage',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                            SizedBox(height: height * 0.02),
-
-                            /// Coupon Code
-                            Text(
-                              'Coupon Code',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                            Text(
-                              '926F',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                          ],
-                        ),
-
-                        /// Value and start date and end date
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            /// Value
-                            Text(
-                              'Value',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                            Text(
-                              '1600',
-                              style: TextStyle(
-                                color: Colors.green,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Monserat',
-                              ),
-                            ),
-                            SizedBox(height: height * 0.02),
-
-                            /// Start Date End Date
-                            Row(
-                              spacing: 14,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Start Date',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w400,
-                                        fontFamily: 'Monserat',
-                                      ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.calendar_month_rounded,
-                                          color: Colors.grey,
-                                          size: 16,
-                                        ),
-                                        Text(
-                                          '12/12/2025',
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'Monserat',
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'End Date',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w400,
-                                        fontFamily: 'Monserat',
-                                      ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.calendar_month_rounded,
-                                          color: Colors.grey,
-                                          size: 16,
-                                        ),
-                                        Text(
-                                          '12/12/2025',
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'Monserat',
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => CouponDeleteDialog(
-                          couponId: '',
-                        ),
-                      );
-                    },
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        padding: EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: AppColor.primaryColor,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(14),
-                            bottomLeft: Radius.circular(10),
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.delete_outline_rounded,
-                          color: Colors.white,
-                        ),
+  Widget buildDashboardCard({
+    required String title,
+    required String subtitle,
+    required String value,
+    required String percentage,
+    required Color percentageColor,
+    required IconData icon,
+    required Color iconColor,
+    required String changeText,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(14),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(50),
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "$title ",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[900]),
+                    children: [
+                      TextSpan(
+                        text: "| $subtitle",
+                        style: TextStyle(fontSize: 14, color: Colors.blueGrey),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              );
-            },
-          ),
+                ),
+                Icon(Icons.more_horiz, color: Colors.blueGrey),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: iconColor.withOpacity(0.2),
+                  radius: 20,
+                  child: Icon(icon, color: iconColor),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      value,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey[900]),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          percentage,
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: percentageColor),
+                        ),
+                        Text(
+                          " $changeText",
+                          style:
+                              TextStyle(fontSize: 14, color: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
